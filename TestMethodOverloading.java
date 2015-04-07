afb31e66a953fe9761a7746635ebd081d27d91df
mrifat022 <mrifat022@gmail.com>
2015-04-07 15:18:02 +0700

    TugasPBO2
    Percobaan

---
+++ TestMethodOverloading.java
@@ -0,0 +1,25 @@
+public class TestMethodOverloading {
+    public static int average(int n1, int n2){              // A
+        return (n1+n2)/2;
+    }
+
+    public static double average(double n1, double n2){     // B
+        return (n1+n2)/2;
+    }
+
+    public static int average(int n1, int n2, int n3){      // C
+        return (n1+n2+n3)/3;
+    }
+
+    public static double average(double n1, double n2, double n3, double n4){      // D
+        return (n1+n2+n3+n4)/4;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(average(1, 2));
+        System.out.println(average(1.0, 2.0));
+        System.out.println(average(1, 2, 3));
+        System.out.println(average(1.0, 2));
+        System.out.println(average(1, 2, 3, 4));
+    }
+}
